c1baefc71858e7afda180a351e4a31e28a20eb98
Igor Storchevoy <hellsgor@gmail.com>
2025-02-05 14:01:54 +0400

    Feat[Input]: created component

---
+++ src/ui/Input/Input.jsx
@@ -0,0 +1,22 @@
+import { setId } from '../../helpers/setId';
+import classes from './Input.module.css';
+import PropTypes from 'prop-types';
+
+export const Input = ({ label, value, error, type = 'text', ...restProps }) => {
+  const id = setId();
+
+  return (
+    <div className={classes.input}>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input id={id} value={value} type={type} {...restProps} />
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+Input.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string,
+};
