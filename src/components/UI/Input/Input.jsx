3d2786998b5969863323212d12ced6bc881383bd
Igor Storchevoy <hellsgor@gmail.com>
2025-02-06 14:33:45 +0400

    Update[Control]: updated styles

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -6,14 +6,12 @@ import classes from './Input.module.css';
 import controlClasses from '../Control/Control.module.css';
 
 export const Input = ({ label, value, error, type = 'text', ...restProps }) => {
-  const id = setId();
-
   return (
-    <div className={getClassNames(classes.input, controlClasses.control)}>
-      {label && <label htmlFor={id}>{label}</label>}
-      <input id={id} value={value} type={type} {...restProps} />
+    <label className={getClassNames(classes.input, controlClasses.control)}>
+      {label && <span>{label}</span>}
+      <input value={value} type={type} {...restProps} />
       {error && <p>{error}</p>}
-    </div>
+    </label>
   );
 };
 
